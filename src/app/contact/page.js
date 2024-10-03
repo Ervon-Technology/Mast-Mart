bd8b8b150db5dfa5f60d45dcf3a2c6502a069650
Sambit  Pattanaik <pattanaiksambit80@gmail.com>
2024-10-03 11:18:06 +0530

    updates

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -34,7 +34,7 @@ export default function Contact() {
     <main className="p-8 bg-gray-100 min-h-screen">
       {/* Contact Introduction */}
       <section className="text-center my-16">
-        <h1 className="text-5xl font-extrabold text-green-700">Contact Mast Mart</h1>
+        <h1 className="text-5xl font-bold text-green-700">Contact Mast Mart</h1>
         <p className="text-lg mt-6 max-w-2xl mx-auto text-gray-600">
           Whether you’re a retailer, farmer, or interested in partnering with us, we are always here to assist.
         </p>
